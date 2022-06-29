0e8ef6053e6b5192142357c5ed630727adf9451b
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-06-29 14:48:08 -0400

    Created index.html & script.js Files

---
+++ script.js
@@ -0,0 +1,24 @@
+let numArray = [];
+
+function fizzBuzzGenerator (){
+    for (let i = 1; i < 101; i++) {
+    
+        if (i % 5 == 0 && i % 3 == 0){
+            numArray.push(" FizzBuzz");
+        }
+        else if (i % 5 == 0) {
+            numArray.push(" Buzz");
+        }
+        else if (i % 3 == 0) {
+            numArray.push(" Fizz");
+        }
+        else {
+            numArray.push(` ${i}`);
+        }
+    }
+    console.log(numArray);
+}
+
+fizzBuzzGenerator();
+
+document.querySelector("h3").textContent = numArray;
